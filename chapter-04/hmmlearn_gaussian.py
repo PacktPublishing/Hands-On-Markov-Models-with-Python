4fee50273482dc56ade6385b31e96bda0a08961e
Ankur Ankan <ankurankan@gmail.com>
2018-09-21 19:57:25 +0200

    fixes chapter 4 code and adds data file

--- chapter-04/hmmlearn_gaussian.py
+++ chapter-04/hmmlearn_gaussian.py
@@ -3,29 +3,31 @@ from __future__ import print_function
 
 import datetime
 import numpy as np
+import pandas as pd
 from matplotlib import cm, pyplot as plt
 from matplotlib.dates import YearLocator, MonthLocator
 
-try:
-    from matplotlib.finance import quotes_historical_yahoo_ochl
-except ImportError:
-    # For Matplotlib prior to 1.5.
-    from matplotlib.finance import (
-        quotes_historical_yahoo as quotes_historical_yahoo_ochl
-        )
+# try:
+#     from matplotlib.finance import quotes_historical_yahoo_ochl
+# except ImportError:
+#     # For Matplotlib prior to 1.5.
+#     from matplotlib.finance import (
+#         quotes_historical_yahoo as quotes_historical_yahoo_ochl
+#         )
 
 from hmmlearn.hmm import GaussianHMM
 
 
 print(__doc__)
 
-quotes = quotes_historical_yahoo_ochl(
-            "INTC", datetime.date(1995, 1, 1), datetime.date(2012, 1, 6))
-
+quotes = pd.read_csv('data/yahoofinance-INTC-19950101-20040412.csv',
+                     index_col=0,
+                     parse_dates=True,
+                     infer_datetime_format=True)
 # Unpack quotes
-dates = np.array([q[0] for q in quotes], dtype=int)
-close_v = np.array([q[2] for q in quotes])
-volume = np.array([q[5] for q in quotes])[1:]
+dates = np.array(quotes.index, dtype=int)
+close_v = np.array(quotes.Close)
+volume = np.array(quotes.Volume)[1:]
 
 # Take diff of close value. Note that this makes
 # ``len(diff) = len(close_t) - 1``, therefore, other quantities also
